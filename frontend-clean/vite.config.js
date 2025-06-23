6e0b2ce13bc8dc2ab916633a6d9fa77507b8856e
RafaelGaby <rafaelbaatista@gmail.com>
2025-06-23 10:49:16 +0000

    FIX: Adiciona configs do Vite e JSConfig na pasta correta do frontend

--- frontend-clean/vite.config.js
+++ frontend-clean/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Importe o módulo 'path' do Node.js
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Adicione este bloco para resolver o atalho '@'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
