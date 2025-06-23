60f57b961068caa0c473ed449dd942da4214f006
RafaelGaby <rafaelbaatista@gmail.com>
2025-06-23 10:43:35 +0000

    Adicionar vite.config.js para resolver alias @

--- frontend-clean/vite.config.js
+++ frontend-clean/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path' // Importe o módulo 'path' do Node.js
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Adicione este bloco para resolver o atalho '@'
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
